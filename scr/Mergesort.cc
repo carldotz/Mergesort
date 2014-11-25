04e07a5de90fec4fd468bacb890fc00ff67abfdb
carldotz <carldotz@163.com>
2014-11-25 15:20:47 +0800

    Version 1.0 complete.

--- scr/Mergesort.cc
+++ scr/Mergesort.cc
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <stdlib.h>
+#include <cstring>
 
 using std::cout;
 using std::endl;
@@ -14,7 +15,19 @@ using std::endl;
 const size_t WIDTH = 100;
 
 void mergesort(int *a, size_t size) {
-
+	if(size==1) return;
+	size_t point = size%2? size/2+1 : size/2;
+	mergesort(a, point);
+	mergesort(a+point, size-point);
+	size_t i=0, j=point, k=0;
+	int *t = new int[size];
+	while(k<size) {
+		if((i<point) && (j<size)) t[k++] = a[i] > a[j] ? a[j++] : a[i++];
+		else if(j>=size) t[k++] = a[i++];
+		else if(i>=point) t[k++] = a[j++];
+	}
+	for(size_t i=0;i<size;++i) a[i] = t[i];
+	delete [] t;
 }
 
 void gen_random(int *a, size_t size) {
